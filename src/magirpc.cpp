91e175658639637c8ee09a981dbcf5b03a384581
Ruckard Smith <ruckard.smith@gmail.com>
2020-12-10 22:04:16 +0100

    Boost 1.70+ support ( Common part )
    * magirpc.cpp
    ** Initial Boost 1.70+ support: Adapted from https://github.com/cygnusxi/CurecoinSource/commit/6b93af07005b90e4f6c5785c60c10c36fa52a333
    ** Change deprecated boost::asio::ssl::context sections : Adapted from https://github.com/VROOM-Project/vroom/commit/578e2c1dcc000d888dc09fa489b46b0175f52f3e
    ** Fix class boost::asio::ssl::context has no member named impl : Adapted from https://github.com/gridcoin-community/Gridcoin-Research/commit/14c5880ab357367b9bcc16c83922ea14796f3cdf
    * magirpc.h
    ** Changes adapted from https://github.com/cygnusxi/CurecoinSource/commit/6b93af07005b90e4f6c5785c60c10c36fa52a333
    * m-wallet.pro

--- src/magirpc.cpp
+++ src/magirpc.cpp
@@ -574,7 +574,7 @@ public:
     }
     bool connect(const std::string& server, const std::string& port)
     {
-        ip::tcp::resolver resolver(stream.get_io_service());
+        ip::tcp::resolver resolver(GetIOService(stream));
         ip::tcp::resolver::query query(server.c_str(), port.c_str());
         ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
         ip::tcp::resolver::iterator end;
@@ -680,7 +680,7 @@ static void RPCListen(boost::shared_ptr< basic_socket_acceptor<Protocol, SocketA
                    const bool fUseSSL)
 {
     // Accept connection
-    AcceptedConnectionImpl<Protocol>* conn = new AcceptedConnectionImpl<Protocol>(acceptor->get_io_service(), context, fUseSSL);
+    AcceptedConnectionImpl<Protocol>* conn = new AcceptedConnectionImpl<Protocol>(GetIOServiceFromPtr(acceptor), context, fUseSSL);
 
     acceptor->async_accept(
             conn->sslStream.lowest_layer(),
@@ -772,7 +772,7 @@ void ThreadRPCServer2(void* parg)
 
     asio::io_service io_service;
 
-    ssl::context context(io_service, ssl::context::sslv23);
+    ssl::context context(ssl::context::sslv23);
     if (fUseSSL)
     {
         context.set_options(ssl::context::no_sslv2);
@@ -788,7 +788,7 @@ void ThreadRPCServer2(void* parg)
         else printf("ThreadRPCServer ERROR: missing server private key file %s\n", pathPKFile.string().c_str());
 
         string strCiphers = GetArg("-rpcsslciphers", "TLSv1+HIGH:!SSLv2:!aNULL:!eNULL:!AH:!3DES:@STRENGTH");
-        SSL_CTX_set_cipher_list(context.impl(), strCiphers.c_str());
+        SSL_CTX_set_cipher_list(context.native_handle(), strCiphers.c_str());
     }
 
     // Try a dual IPv6/IPv4 socket, falling back to separate IPv4 and IPv6 sockets
@@ -1082,7 +1082,7 @@ Object CallRPC(const string& strMethod, const Array& params)
     // Connect to localhost
     bool fUseSSL = GetBoolArg("-rpcssl");
     asio::io_service io_service;
-    ssl::context context(io_service, ssl::context::sslv23);
+    ssl::context context(ssl::context::sslv23);
     context.set_options(ssl::context::no_sslv2);
     asio::ssl::stream<asio::ip::tcp::socket> sslStream(io_service, context);
     SSLIOStreamDevice<asio::ip::tcp> d(sslStream, fUseSSL);
