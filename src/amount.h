5d36d2ee11f3d0989851fdbe5ec7fb9e3c4dedac
Ruckard Smith <ruckard.smith@gmail.com>
2020-12-12 00:04:45 +0100

    amount.h: COINS_BURNED recovered
    This sort of reverts 'For now disable coin burn' commit ( 345d78ebc468bb40d45a05b8c0355fa910eec080 ).
    
    The only purpose of this commit is to be able to build the project.
    
    Removing COINS_BURNED I have no idea if it was a good idea or not.

--- src/amount.h
+++ src/amount.h
@@ -14,10 +14,10 @@ typedef int64_t CAmount;
 static const CAmount COIN = 100000000;
 static const CAmount CENT = 1000000;
 
-// static const int COINS_BURNED = 720000 * COIN; // Notes: https://bitcointalk.org/index.php?topic=735170.msg9475622#msg9475622
+static const int COINS_BURNED = 720000 * COIN; // Notes: https://bitcointalk.org/index.php?topic=735170.msg9475622#msg9475622
 
 
-static const CAmount MAX_MONEY = 25000000 * COIN; // Magi MAX supply
+static const CAmount MAX_MONEY = 25000000 * COIN + COINS_BURNED; // Magi MAX supply
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 #endif //  BITCOIN_AMOUNT_H
